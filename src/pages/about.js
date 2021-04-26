23b1e94b2e6010189fa1d0543a0586a79f9930e6
jwkim <phoongdeng@gmail.com>
2021-04-26 23:57:45 +0900

    Add Tutorial Part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,23 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Header from "../components/header"
 import Layout from "../components/layout"
 
-export default function About() {
+export default function About({ data }) {
   return (
     <Layout>
-      <Header headerText="About Gatsby" />
-      <Header headerText="It's pretty cool" />
+      <Header headerText={data.site.siteMetadata.title} />
       <p>Such wow. Very React.</p>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
