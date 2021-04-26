d3d110cd36509b50b2cd553d168d9afde7e25307
jwkim <phoongdeng@gmail.com>
2021-04-27 02:11:43 +0900

    Add Tutorial Part 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Header from "../components/header"
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
