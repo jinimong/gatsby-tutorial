883321a8f2137e3a03a481cb488dd7946fa6865e
jwkim <phoongdeng@gmail.com>
2021-04-27 00:07:14 +0900

    Add Tutorial Part 5

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,6 +29,7 @@ export default function Layout({ children }) {
           <ListLink to="/">Home</ListLink>
           <ListLink to="/about/">About</ListLink>
           <ListLink to="/contact/">Contact</ListLink>
+          <ListLink to="/my-files/">My Files</ListLink>
         </ul>
       </header>
       {children}
